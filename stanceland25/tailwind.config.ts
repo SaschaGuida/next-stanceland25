15f34af65c55740e2495784bf445ccb4935d6d1c
sak89 <sascha.guida@gmail.com>
2025-02-06 09:19:59 +0100

    v2

--- stanceland25/tailwind.config.ts
+++ stanceland25/tailwind.config.ts
@@ -1,18 +1,43 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+const config: Config = {
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        trueGray: colors.neutral,
+        indigo: {
+          50: "#FEECE6",
+          100: "#FDC9B8",
+          200: "#FC9A81",
+          300: "#FA6B4B",
+          400: "#F94A27",
+          500: "#ED4821", // Colore principale
+          600: "#D33D1B",
+          700: "#B83316",
+          800: "#8F2710",
+          900: "#661B0B",
+        },
       },
     },
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      stock: [defaultTheme.fontFamily.sans],
+    },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
